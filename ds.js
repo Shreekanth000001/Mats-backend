e17469ac18df9ebe329e662a8b5d4669754225f1
Shreekanth.k <Shreekanth.k000001@gmail.com>
2025-02-06 20:09:49 +0530

    update

--- ds.js
+++ ds.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // require('.env').config();
 
-const mongoURI = "mongodb+srv://shreekanthk000001:shreekanthadmin1@matsdb.s22pl.mongodb.net/mats";
+const mongoURI = process.env.mongouri;
 
 const connecToMongo = async () => {
     await mongoose.connect(mongoURI)
