610a85f0a751feae46f44b020528f0dffe81fd9e
Shreekanth.k <Shreekanth.k000001@gmail.com>
2025-02-07 15:42:55 +0530

    ..

--- ds.js
+++ ds.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 
-// require('.env').config();
-
-const mongoURI = "mongodb+srv://shreekanthk000001:shreekanthadmin1@matsdb.s22pl.mongodb.net/mats";
-
+const mongoURI = process.env.mongouri;
 const connecToMongo = async () => {
-    await mongoose.connect(mongoURI)
-        .then(() => {
-            console.log('success');
-        }).catch((err) => {
-            console.log('no success', err);
-        });
+    if (!mongoURI) {
+        console.error("❌ MONGO_URI is undefined. Make sure it is set in Koyeb.");
+        return;
+    }
+
+    await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+        autoIndex: false
+    })
+    .then(() => console.log(" Connected to MongoDB"))
+    .catch((err) => console.error(" MongoDB connection error:", err));
 }
 module.exports = connecToMongo
